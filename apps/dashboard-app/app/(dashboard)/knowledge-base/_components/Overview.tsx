05688b87e7775f4953963173f6c9c1991849aed5
anoopkarnik <danoopkarnik@gmail.com>
2024-08-03 05:18:30 +0530

    Added Planner and Financial Forms

--- apps/dashboard-app/app/(dashboard)/knowledge-base/_components/Overview.tsx
+++ apps/dashboard-app/app/(dashboard)/knowledge-base/_components/Overview.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ConnectionsContext } from '../../../../providers/connections-provider'
 
 const Overview = () => {
+
+  const connectionsContext = useContext(ConnectionsContext);
+  const apiToken = connectionsContext?.notionNode?.accessToken
+  
+
   return (
     <div>Overview</div>
   )
