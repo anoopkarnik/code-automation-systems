3665ba3c49523988fb0c8897b6f61f5669b3e1fc
anoopkarnik <danoopkarnik@gmail.com>
2024-08-21 05:44:06 +0530

    UX design improvements

--- apps/dashboard-app/app/(dashboard)/skill-trees/_components/AreaCard.tsx
+++ apps/dashboard-app/app/(dashboard)/skill-trees/_components/AreaCard.tsx
@@ -16,17 +16,17 @@ const AreaCard = ({page,type,testGrades,revisionTimeTaken}:any) => {
       };
   return (
     <div  className={cn(" p-4 my-2 mr-4 rounded-xl",
-        (new Date() >= new Date(page['Review Date']) ) ? "bg-destructive/20" : "")} >
+        (new Date() >= new Date(page['Review Date']) ) ? "bg-destructive/10" : "")} >
           <div className='flex items-center justify-start gap-2 flex-wrap'>
             {page.Name}
            <div className='font-extralight text-sm'>{page['Review Date'] ? format(page['Review Date'], "dd MMMyy") : ''} | </div>
            <div className='font-extralight text-sm'>{page?.['Self Grade Name']} | </div>
            <div className='font-extralight text-sm'>{page?.['Difficulty Name']} | </div>
            <div className='font-extralight text-sm'>{page?.['Total Line Count'].match(/\d+/)[0]} lines | </div>
-           <div className='font-extralight text-sm'> {page?.['Repetitions']} repetitions done</div>
+           <div className='font-extralight text-sm'> {page?.['Repetitions']} revisions done</div>
           </div>
           <div className='font-extralight text-sm'>{page.Description ? page.Description : 'No Description Available'}</div>
-          <div className='mt-2'>
+          <div className='mt-2 flex items-center gap-4'>
             {type==="Notes" && <Button variant='outline' onClick={() => calculateAndUpdateNextRevisionDate(page,apiToken)}>Revision Done</Button>}
             <Button variant='outline' onClick={() => handlePageClick(page)}>Open Notion Page</Button>
           </div>
