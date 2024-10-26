2fab711736308b4ae7bd78edc68d6c8c5cea04b5
anoopkarnik <danoopkarnik@gmail.com>
2024-10-26 09:31:31 +0530

    Added error in python code execution and kafka increased poll seconds

--- apps/flask-worker/app/main/services/kafka.py
+++ apps/flask-worker/app/main/services/kafka.py
@@ -27,6 +27,7 @@ def consume_kafka_messages():
         auto_offset_reset='earliest',
         enable_auto_commit=False,
         group_id='flask-kafka-group',
+        max_poll_interval_ms=600000,
         value_deserializer=lambda x: json.loads(x.decode('utf-8'))
     )
     producer = KafkaProducer(
